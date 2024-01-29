2c856c57043f0094ddb36cccec20bd4c88b6ec53
AutofillMe <jakosnjpn@gmail.com>
2024-01-29 14:57:47 -0500

    testing.py

---
+++ testing.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def sort(list):
+    """
+    Quicksort recursive algorithm
+
+    Input: list (min len = 0, max len = n)
+
+    Output: list (len = n)
+    """
+    if len(list) <= 1:
+        return list
+
+    randomPivot = list[0]
+    pivot = [i for i in list if i == randomPivot]
+    left = [i for i in list if i < randomPivot]
+    right = [i for i in list if i > randomPivot]
+
+    list = sort(left) + pivot + sort(right)
+    return list
+
+randlist = np.random.randint(1, 100, 20)
+sorted = sort(randlist)
+print(sorted)
+help(sort)
