55a67732e398ce45adbc4e02c84f7957ea356019
AutofillMe <jakosnjpn@gmail.com>
2024-03-11 11:17:27 -0400

    added back in removing empty emails, testing NaN

--- testing.py
+++ testing.py
@@ -9,36 +9,69 @@
 # TODO: Comment
 # //Grossed Out Comment
 
-# Join the discord: https://discord.gg/6BdusdsB
+import re
 
-import numpy as np
-import time
+import pandas as pd
 
-def sort(list):
-    """
-    Quicksort recursive algorithm
+# Reading in the data
+df_phish = pd.read_csv("Phishing_Email.csv")
+# Dropping the unecessary index row
+df_phish = df_phish.drop(df_phish.columns[0], axis=1)
+# checking that it read in correctly
+df_phish[0:10]
 
-    Input: list (min len = 0, max len = n)
+NullCount = df_phish["Email Text"].isnull().sum()
+print("Nulls:", NullCount)
+# Check for null values in the "Email Text" column
+null_mask = df_phish["Email Text"].isnull()
 
-    Output: list (len = n)
-    """
-    if len(list) <= 1:
-        return list
+# If there are null values, drop the corresponding rows
+if null_mask.any():
+    df_phish = df_phish.drop(df_phish[null_mask].index)
+# Checking if null values were dropped
+NullCount = df_phish["Email Text"].isnull().sum()
+print("Nulls:", NullCount)
 
-    randomPivot = list[0]
-    pivot = [i for i in list if i == randomPivot]
-    left = [i for i in list if i < randomPivot]
-    right = [i for i in list if i > randomPivot]
+# Visual Inspection
+print(df_phish.head(10))
+print(df_phish.tail(20))
 
-    list = sort(left) + pivot + sort(right)
-    return list
+temp = input()
 
-randlist = np.random.randint(1, 100, 2000)
-start = time.perf_counter()
-sorted = sort(randlist)
-end =  time.perf_counter()
-duration = end - start
-print(f"Quicksort took {duration:.4f} seconds.")
-print(randlist[:20])
-print(sorted[:20])
-help(sort)
+# Check if each "Email Text" is type string
+print(df_phish.shape)
+# Check if each "Email Text" is type string
+is_string = df_phish["Email Text"].apply(lambda x: isinstance(x, str))
+print("Number of non-string values in 'Email Text':", (~is_string).sum())
+print(df_phish.shape)
+
+temp = input()
+
+# Get count of 'empty' emails
+empty_email_count = df_phish["Email Text"].str.lower().eq("empty").sum()
+print(f"Number of 'empty' emails: {empty_email_count}")
+# Remove those 'empty' emails from the dataframe
+df_phish_cleaned = df_phish[df_phish["Email Text"].str.lower() != "empty"]
+df_phish_cleaned.shape
+empty_email_count = df_phish_cleaned["Email Text"].str.lower().eq("empty").sum()
+print(f"Number of 'empty' emails: {empty_email_count}")
+df_phish = df_phish_cleaned
+
+temp = input()
+
+print(df_phish.tail(5))
+countNAN = df_phish["Email Text"] == "NaN"
+print(countNAN.sum())
+
+temp = input()
+
+print(df_phish.tail(20))
+# Adding "Is_Response" feature
+df_phish["Is_Response"] = None
+# Looping over the DataFrame to determine "Is_Response"
+for i in range(df_phish.shape[0]):
+    has_re = re.search("re :", df_phish.iloc[i]["Email Text"])
+    df_phish.at[i, "Is_Response"] = bool(has_re)
+print(df_phish.tail(20))
+
+df_phish["Is_Response"].value_counts(normalize=True)
